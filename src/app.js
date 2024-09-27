8d52a4bf28e29722e12e2cdad236f9a83fe2256b
Ajinkya Khandar <Ajinkya.Khandar@in.ey.com>
2024-09-28 02:45:04 +0530

    [session 01] creacet server and install nodemon

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("starting new project ");
+const express = require('express'); // Import the express module
+const app = express(); // Create an instance of an express application
+
+// Middleware that runs on every request
+app.use("/test",(req, res, next) => {
+    res.send(`${req.method} request called`)
+});
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
